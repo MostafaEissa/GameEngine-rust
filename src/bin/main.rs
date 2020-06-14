82af2ad195b36265757a5ff7a4e907ff80bcc747
unknown <mostafamohamad@hotmail.com>
2020-06-14 13:32:07 +0200

    Making A Game #2 : Drawing Textures/Sprites To The Screen

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,10 +5,6 @@ extern crate game_engine;
 fn main() {
 	let mut game = game_engine::Game::init("GameEngine", 800, 600, false).unwrap();
 
-	while game.running() {
-		game.handle_events();
-		game.update();
-		game.render();
-	}
+	game.start();
 }
 
--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,15 @@
+use sdl2::Sdl;
+use sdl2::image::LoadSurface;
+use sdl2::render::{Canvas, Texture};
+use sdl2::video::{Window};
+
 pub struct Game {
 	is_running: bool,
-	renderer: sdl2::render::Canvas<sdl2::video::Window>,
-	sdl_context: sdl2::Sdl
+    renderer: Canvas<Window>,
+    sdl_context: Sdl,
+
+    cnt: i32,
+    dest_rect: sdl2::rect::Rect
 }
 
 impl Game {
@@ -28,29 +36,55 @@ impl Game {
 		println!("Window created!...");
 		
 		// create renderer
-		let mut renderer = window.into_canvas().build().map_err(|err| err.to_string())?;
+        let mut renderer = window.into_canvas().build().map_err(|err| err.to_string())?;
 		println!("Renderer created!...");
 
 		// set drawing color
-		renderer.set_draw_color(sdl2::pixels::Color::WHITE);
+        renderer.set_draw_color(sdl2::pixels::Color::WHITE);
 
-		Ok(Game {is_running: true, sdl_context: sdl, renderer: renderer})
-	}
+        Ok(Game {
+            is_running: true, 
+            sdl_context: sdl, 
+            renderer: renderer,
+            cnt: 0,
+            dest_rect: sdl2::rect::Rect::new(0, 0, 64, 64)
+        })
+    }
 
-	pub fn render(&mut self) {
+    pub fn start(&mut self) {
+
+        //create player textures
+        let texture_creator = self.renderer.texture_creator();
+        let surface = sdl2::surface::Surface::from_file("assets/player.png").unwrap();
+        let player_texture = surface.as_texture(&texture_creator).unwrap();
+
+        while self.running() {
+            self.handle_events();
+            self.update();
+            self.render(&player_texture);
+        }
+    }
+
+	fn render(&mut self, player_texture: &Texture) {
 		// clear screen
 		self.renderer.clear();
+
+        // draw player
+        self.renderer.copy(player_texture, None, self.dest_rect).unwrap();
 
 		// render on screen
 		self.renderer.present();
 	
 	}
 
-	pub fn update(&mut self) {
+	fn update(&mut self) {
+        println!("{}", self.cnt);
+        self.cnt += 1;
 
+        self.dest_rect.set_x(self.cnt);
     }
 
-	pub fn handle_events(&mut self) {
+	fn handle_events(&mut self) {
 		let mut event_pump = self.sdl_context.event_pump().unwrap();
 		if let Some(event) = event_pump.poll_event() {
 			match event {
@@ -66,5 +100,5 @@ impl Game {
 
 	fn set_running(&mut self, running: bool) {
 		self.is_running = running;
-	}
+    }
 }
